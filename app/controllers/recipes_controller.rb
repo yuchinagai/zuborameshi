9cc4b10a9e77a2ea03568cfcf8ef567e62472ab1
YuchiNagai <alone.on.the.rope1231@gmail.com>
2019-03-20 14:25:03 +0900

    ヘッダーの作成

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,2 +1,21 @@
 class RecipesController < ApplicationController
+  def index
+    @recipe = Recipe.all
+  end
+
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    @recipe.save
+    redirect_to @recipe, notice: "レシピを登録しました。"
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:title, :material, :body, :point)
+  end
 end
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# 静的なファイルをRailsから返す
+config.public_file_server.enabled = true
