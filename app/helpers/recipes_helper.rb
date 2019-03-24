119b725b182f4fceec7ba9637677a57df370b5e4
YuchiNagai <alone.on.the.rope1231@gmail.com>
2019-03-24 17:58:59 +0900

    カルーセル機能の追加

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -6,6 +6,7 @@ module RecipesHelper
       image_tag "img_warning.png", class: "img-thumbnail"
     end
   end
+
   def show_recipe_small_image(recipe)
     if recipe&.image&.attached?
       image_tag recipe.image.variant(resize: "50x50"), class: "img-thumbnail"
@@ -13,4 +14,13 @@ module RecipesHelper
       image_tag "img_warning.png", class: "img-thumbnail", width: 50
     end
   end
+
+  def calusel1
+    "https://i.pinimg.com/originals/1f/aa/c3/1faac3b711a8366d4c42bc2d2dbf79cd.png"
+  end
+  def w
+    @carouselslide2
+    @carouselslide3
+  end
+
 end
