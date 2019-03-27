e285cb3aa069be94190d96fa78966faabbba21c5
YuchiNagai <alone.on.the.rope1231@gmail.com>
2019-03-27 14:04:54 +0900

    カテゴリ一覧ページの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root "recipes#index"
-  get "/about" => "recipes#about"
   get "recipes/all" => "recipes#all"
   devise_for :users
   resources :recipes do
